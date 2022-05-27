d5ca0523416511a94883c07898ee1a653891fa94
PhantomNeon <guptaishaan06@gmail.com>
2022-05-27 13:10:50 +0530

    Initial Commit

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ManageX.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
