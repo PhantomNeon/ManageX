887236e9e844fe1830005c0c72392b80914911a2
PhantomNeon <guptaishaan06@gmail.com>
2022-05-31 17:09:22 +0530

    Confusing myself speedrun any%

--- Models/UserDataModel.cs
+++ Models/UserDataModel.cs
@@ -9,13 +9,28 @@ namespace ManageX.Models;
 public class UserDataModel
 {
     public ObservableCollection<string> Headers { get; set; }
-    public ObservableCollection<string> HeaderData { get; set; }
+    public ObservableCollection<ObservableCollection<string>> HeaderData { get; set; }
 
-    public void GetUserDataFromJson()
+    public UserDataModel()
+    {
+        Headers = JArrayToObservableCollection<string>(GetDataFromJson("Header"));
+        GetIndividualDataFromJson();
+    }
+
+    public JArray GetDataFromJson(string title)
     {
         dynamic jsonData = JsonConvert.DeserializeObject(File.ReadAllText("Services/UserData.json"))!;
-
+        return jsonData[title];
     }
+
+    public void GetIndividualDataFromJson()
+    {
+        foreach (string header in Headers)
+        {
+            HeaderData.Add(JArrayToObservableCollection<string>(GetDataFromJson("header")));
+        }
+    }
+    
 
     private static ObservableCollection<T> JArrayToObservableCollection<T>(JArray array)
     {
